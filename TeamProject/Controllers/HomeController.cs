6a48b5d709599c3be140fb22cf595a1a6fcaca58
inthedark <inthedark8@gmail.com>
2016-12-22 11:35:00 +0200

    Registation with bag

--- TeamProject/Controllers/HomeController.cs
+++ TeamProject/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DomainModel.Concrete;
+using DomainModel.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,12 @@ namespace TeamProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserRepository userRepository;
+        public HomeController()
+        {
+            EFContext context = new EFContext();
+            userRepository = new UserRepository(context);
+        }
         public ActionResult Index()
         {
             return View();
@@ -20,7 +28,15 @@ namespace TeamProject.Controllers
         [HttpPost]
         public ActionResult Registration(RegistrationViewModel model)
         {
-
+            if (userRepository.CreateUser(model.email,model.Password,model.name,model.surname,model.address,model.phone))
+            {
+                return RedirectToAction("index", "home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Error");
+                return View();
+            }
         }
     }
 }
