3826fc8404137a5329b36fe0f166ed8e9f6e6dcd
user4545 <vlad777427@gmail.com>
2017-01-19 02:17:26 +0200

    Add Payment Controller

---
+++ TeamProject/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace TeamProject.Controllers
+{
+     public class PaymentController
+     {
+          // GET: Payment
+          public ActionResult Index()
+          {
+               return View();
+          }
+     }
+}
